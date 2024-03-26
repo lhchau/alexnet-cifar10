bc732310b2ea20bebfb966b92967de7b82362537
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-03-26 16:13:34 +0700

    fix last layer vgg & add custom vgg23

--- model/__init__.py
+++ model/__init__.py
@@ -5,24 +5,26 @@ from .vgg import *
 
 
 def get_model(
-    name='alexnet',
+    model_name='alexnet',
     num_classes=10,
     activation='relu',
     dropout=True
 ):
-    if name == 'alexnet':
+    if model_name == 'alexnet':
         return AlexNet(num_classes=num_classes, activation=activation, dropout=dropout)
-    elif name == 'minimal_alexnet':
+    elif model_name == 'minimal_alexnet':
         return Minimal_Alexnet(num_classes=num_classes, activation=activation, dropout=dropout)
-    elif name == 'chaunet':
+    elif model_name == 'chaunet':
         return ChauNet(num_classes=num_classes, activation=activation, dropout=dropout)
-    elif name == 'vgg11':
+    elif model_name == 'vgg11':
         return vgg11_bn(num_classes=num_classes, activation=activation)
-    elif name == 'vgg13':
+    elif model_name == 'vgg13':
         return vgg13_bn(num_classes=num_classes, activation=activation)
-    elif name == 'vgg16':
+    elif model_name == 'vgg16':
         return vgg16_bn(num_classes=num_classes, activation=activation)
-    elif name == 'vgg19':
+    elif model_name == 'vgg19':
         return vgg19_bn(num_classes=num_classes, activation=activation)
+    elif model_name == 'vgg23':
+        return vgg23_bn(num_classes=num_classes, activation=activation)
     else:
         raise ValueError("Only support options: alexnet")
